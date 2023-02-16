54320bcdda46607532ea03414b01a8509b0311df
matthoot <matt221136@gmail.com>
2023-02-15 21:51:48 -0500

    added ghost check collision

--- pacman.py
+++ pacman.py
@@ -105,8 +105,64 @@ class Ghost:
         return ghost_rect
 
     def check_collisions(self):
+        num1 = ((HEIGHT - 50) // 32)
+        num2 = WIDTH // 30
+        num3 = 15
         self.turns = [False, False, False, False]
-        self.in_box = True
+        if self.center_x // 30 < 29:
+            if level[self.center_y // num1][(self.center_x - num3) // num2] < 3 \
+                    or level[self.center_y // num1][(self.center_x - num3) // num2] == 9 and (self.in_box or self.dead):
+                self.turns[1] = True
+            if level[self.center_y // num1][(self.center_x + num3) // num2] < 3 \
+                    or level[self.center_y // num1][(self.center_x + num3) // num2] == 9 and (self.in_box or self.dead):
+                self.turns[0] = True
+            if level[(self.center_y + num3) // num1][self.center_x // num2] < 3 \
+                    or level[self.center_y + num3 // num1][self.center_x // num2] == 9 and (self.in_box or self.dead):
+                self.turns[3] = True
+            if level[(self.center_y + num3) // num1][self.center_x // num2] < 3 \
+                    or level[self.center_y - num3// num1][self.center_x // num2] == 9 and (self.in_box or self.dead):
+                self.turns[2] = True
+            if self.direction == 2 or self.direction == 3:
+                if 12 <= self.center_x % num2 <= 18:
+                    if level[(self.center_y + num3) // num1][self.center_x // num2] < 3 \
+                        or (level[(self.center_y + num3) // num1][self.center_x// num2] == 9 and (self.in_box or self.dead)):
+                        self.turns[3] = True
+                    if level[(self.center_y - num3) // num1][self.center_x // num2] < 3 \
+                        or (level[(self/center_y - num3) // num1][self.center_x// num2] == 9 and (self.in_box or self.dead)):
+                        self.turns[2] = True
+                if 12 <= self.center_y % num1 <= 18:
+                    if level[self.center_y // num1][(self.center_x - num2) // num2] < 3 \
+                        or (level[self.center_y // num1][(self.center_x - num2)// num2] == 9 and (self.in_box or self.dead)):
+                        self.turns[1] = True
+                    if level[self.center_y // num1][(self.center_x + num2) // num2] < 3 \
+                        or (level[self.center_y // num1][(self.center_x + num2)// num2] == 9 and (self.in_box or self.dead)):
+                        self.turns[0] = True
+                
+                        
+            if self.direction == 0 or self.direction == 1:
+                if 12 <= self.center_x % num2 <= 18:
+                    if level[(self.center_y + num3) // num1][self.center_x // num2] < 3 \
+                        or (level[(self.center_y + num3) // num1][self.center_x// num2] == 9 and (self.in_box or self.dead)):
+                        self.turns[3] = True
+                    if level[(self.center_y - num3) // num1][self.center_x // num2] < 3 \
+                        or (level[(self.center_y - num3) // num1][self.center_x// num2] == 9 and (self.in_box or self.dead)):
+                        self.turns[2] = True
+                if 12 <= self.center_y % num1 <= 18:
+                    if level[self.center_y // num1][(self.center_x - num3) // num2] < 3 \
+                        or (level[self.center_y // num1][(self.center_x - num3)// num2] == 9 and (self.in_box or self.dead)):
+                        self.turns[1] = True
+                    if level[self.center_y // num1][(self.center_x + num3) // num2] < 3 \
+                        or (level[self.center_y // num1][(self.center_x + num3)// num2] == 9 and (self.in_box or self.dead)):
+                        self.turns[0] = True
+        else:
+            self.turns[0] = True
+            self.turns[1] = True
+        if 350 < self.x_pos < 550 and 370 < self.y_pos < 490:
+            self.in_box = True
+        else:
+            self.in_box = False
+            
+                
         return self.turns, self.in_box
 
 def drawboard(level):
